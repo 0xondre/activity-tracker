1dc88664445618f4829eec445f9d904f19f30cad
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2023-08-02 21:23:36 +0200

    Extended some ideas about project

--- src/main/java/com/ondre/calendar/Calendar.java
+++ src/main/java/com/ondre/calendar/Calendar.java
@@ -1,2 +1,6 @@
-package org.example.calendar;public class Calendar {
+package com.ondre.calendar;
+
+public interface Calendar {
+    public void addEvent(int year, int month, int day, int hour, int minute, String name);
+
 }
--- src/main/java/com/ondre/calendar/CalendarGoogle.java
+++ src/main/java/com/ondre/calendar/CalendarGoogle.java
@@ -1,2 +1,6 @@
-package com.ondre.calendar;public class CalendarGoogle {
+package com.ondre.calendar;
+
+public class CalendarGoogle implements Calendar{
+    public void addEvent(int year, int month, int day, int hour, int minute, String name) {
+    }
 }

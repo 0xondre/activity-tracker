aacfa2bbeb13afd0e59cc4fb9d2a94385385a6ce
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2023-08-08 12:37:58 +0200

    modified:   src/main/java/com/ondre/calendar/CalendarGoogle.java 	modified:   .gitignore 	modified:   .idea/misc.xml 	modified:   pom.xml 	modified:   src/main/java/com/ondre/Main.java 	modified:   src/main/java/com/ondre/calendar/CalendarModel.java

--- src/main/java/com/ondre/Main.java
+++ src/main/java/com/ondre/Main.java
@@ -1,10 +1,13 @@
 package com.ondre;
 
 import com.ondre.activity.Activity;
+import com.ondre.calendar.CalendarGoogle;
 
 public class Main {
     public static void main(String[] args) {
         Activity activity = new Activity();
-        activity.sortProcessList();
+        System.out.println(activity.sortProcessList());
+        CalendarGoogle calendarGoogle = new CalendarGoogle();
+        calendarGoogle.addEvent(activity.sortProcessList());
     }
 }

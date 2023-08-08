6ba0d86614eecd354bef7150984b4a7ef83273ae
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2023-08-08 12:37:08 +0200

    addEvent() method made

--- src/main/java/com/ondre/calendar/CalendarGoogle.java
+++ src/main/java/com/ondre/calendar/CalendarGoogle.java
@@ -1,27 +1,36 @@
 package com.ondre.calendar;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventDateTime;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.Collections;
 
 
 public class CalendarGoogle implements CalendarModel {
     private static final String APPLICATION_NAME = "activity-tracker";
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens"; // co to kurwa znaczy?
 
-    private final String calendarId = "primary";
     //("https://www.googleapis.com/calendar/v3/calendars/%s/events", calendarId)
 
-    public void addEvent() {
+    public void addEvent(String summary) {
         try {
             HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
             Credential credential = getCredentials(httpTransport);
@@ -31,21 +40,36 @@ public class CalendarGoogle implements CalendarModel {
                     .build();
 
             Event event = new Event()
-                    .setSummary("Google I/O 2015")
+                    .setSummary(summary)
                     .setStart(new EventDateTime()
                             .setDateTime(new DateTime("2023-08-07T09:00:00-07:00"))
                             .setTimeZone("Europe/Prague"))
                     .setEnd(new EventDateTime()
                             .setDateTime(new DateTime(System.currentTimeMillis()+3600000))
                             .setTimeZone("Europe/Prague"));
+            String calendarId = "primary";
             event = service.events().insert(calendarId, event).execute();
             System.out.printf("Event created: %s\n", event.getHtmlLink());
 
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (GeneralSecurityException | IOException e) {
             throw new RuntimeException(e);
         }
     }
+    private static Credential getCredentials(HttpTransport httpTransport) throws IOException {
+        InputStream in = CalendarGoogle.class.getResourceAsStream("/credentials.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, JSON_FACTORY, clientSecrets,
+                Collections.singletonList(CalendarScopes.CALENDAR))
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        return new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver.Builder().setPort(8888).build())
+                .authorize("user");
+    }
+
 
 }
